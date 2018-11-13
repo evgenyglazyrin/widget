266a3040c9fe132ec71ecfe5412e8644961ea559
evgenyglazyrin <evgeny.glazyrin87@gmail.com>
2018-11-13 13:26:45 +0300

    added table user

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -16,6 +16,9 @@ $this->title = 'My Yii Application';
 
     <div class="body-content">
 
+        <?= \yii\bootstrap\Html::a('виджет бронирования', ['site/index'], ['class'=> 'btn btn-success'])?>
+
+
         <div class="row">
             <div class="col-lg-4">
                 <h2>Heading</h2>
